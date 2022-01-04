40c4bc628f42416061647cfc5c8c5338fa8caef0
Mateusz <44598863+kchn9@users.noreply.github.com>
2022-01-04 18:40:38 +0100

    Update Wallet.sol
    Add depositFunds function and debug only getter

--- Wallet.sol
+++ Wallet.sol
@@ -2,4 +2,18 @@
 pragma solidity ^0.8.0;
 
 contract Wallet {
+
+    /**
+     * @dev Debug only contract balance getter function
+     */
+    function getWalletBalance() external view returns(uint) {
+        return address(this).balance;
+    }
+
+    /**
+     * @dev Deposit sender Ether funds in wallet
+     */
+    function depositFunds() payable public {
+        payable(this).transfer(msg.value);
+    }
 }
