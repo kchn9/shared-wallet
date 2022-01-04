67c0593d7dc7fa5a0790e09bbed99ca5cf725a46
Mateusz <44598863+kchn9@users.noreply.github.com>
2022-01-04 21:41:03 +0100

    Add events and deprecate renounceOwnership()

--- Allowable.sol
+++ Allowable.sol
@@ -9,8 +9,14 @@ pragma solidity ^0.8.0;
  */
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
+/**
+ * @dev Allowable implements mechanism to control access to any countable goods by 
+ * 'onlyAllowed' modifier.
+ */
 contract Allowable is Ownable {
     mapping (address => uint) private allowance; // allowance ledger
+
+    event AllowanceChanged(address indexed _who, address _by, uint _oldBalance, uint _newBalance);
 
     /**
      * @dev Allows owner or anyone with enough allowance to call specified functions
@@ -24,6 +30,7 @@ contract Allowable is Ownable {
      * @dev Sets allowance to specified amount, only owner may call it
      */
     function setAllowance(address _who, uint _allowance) public onlyOwner {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], _allowance);
         allowance[_who] = _allowance;
     }
 
@@ -31,8 +38,9 @@ contract Allowable is Ownable {
      * @dev Reduces allowance by specified amount
      * @param _amount Amount of ether allowance to reduce
      */
-    function reduceAllowance(uint _amount) internal onlyAllowed(_amount) {
+    function reduceAllowance(address _who, uint _amount) internal onlyAllowed(_amount) {
         require(_amount <= allowance[msg.sender], "Allowable: Amount to reduce allowance is higher than allowance");
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who] -= _amount);
         allowance[msg.sender] -= _amount;
     }
 }
--- Wallet.sol
+++ Wallet.sol
@@ -2,13 +2,13 @@
 
 pragma solidity ^0.8.0;
 
-/**
- * @dev Allowable implements mechanism to control access to any countable goods by
- * 'onlyAllowed' modifier.
- */
 import "./Allowable.sol";
 
 contract Wallet is Allowable {
+
+    event FundsReceived(address indexed _from, uint _amount);
+    event FundsSent(address indexed _to, uint _amount);
+
     /**
      * @dev Get contract balance
      */
@@ -25,8 +25,9 @@ contract Wallet is Allowable {
     function withdrawFunds(address payable _to, uint _amount) external onlyAllowed(_amount) {
         require(_amount <= address(this).balance, "Wallet: Not enough funds"); // validate input
         if (msg.sender != owner()) {
-            reduceAllowance(_amount); // reduce allowance to avoid double-spending
+            reduceAllowance(msg.sender, _amount); // reduce allowance to avoid double-spending
         } // do not reduce allowance of owner
+        emit FundsSent(_to, _amount);
         _to.transfer(_amount);
     }
 
@@ -35,11 +36,20 @@ contract Wallet is Allowable {
      */
     function depositFunds() payable public {
         payable(this).transfer(msg.value);
+        emit FundsReceived(msg.sender, msg.value);
     }
 
     /**
      * @dev Fallback function to receive sender Ether
      */
     receive() external payable {
+        emit FundsReceived(msg.sender, msg.value);
+    }
+
+    /**
+     * @dev Deprecating renounceOwnership() from Ownable.sol, due there is no point to use it
+     */
+    function renounceOwnership() override public view onlyOwner {
+        revert("Wallet: Ownership renouncing is not allowed.");
     }
 }
