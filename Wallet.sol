bacf49eea62f94118fbcfbcee432f7f50ea9b5ad
Mateusz <44598863+kchn9@users.noreply.github.com>
2022-01-04 18:56:24 +0100

    Update Wallet.sol
    Add withdraw function

--- Wallet.sol
+++ Wallet.sol
@@ -11,6 +11,16 @@ contract Wallet {
     }
 
     /**
+     * @dev Withdraws wallet funds to specified address
+     * @param _to Address to receive funds
+     * @param _amount Amount of funds to withdraw
+     */
+    function withdrawFunds(address payable _to, uint _amount) external {
+        require(address(this).balance <= _amount); // validate input
+        _to.transfer(_amount);
+    }
+
+    /**
      * @dev Deposit sender Ether funds in wallet
      */
     function depositFunds() payable public {
