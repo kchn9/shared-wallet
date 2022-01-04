91a4d598c458f6911751eb9e503e50c59636f718
Mateusz <44598863+kchn9@users.noreply.github.com>
2022-01-04 19:56:42 +0100

    Update Wallet.sol
    Delete redundant code & import Ownable contract from OpenZeppelin

--- Wallet.sol
+++ Wallet.sol
@@ -1,24 +1,15 @@
 //SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
-contract Wallet {
+/**
+ * @dev Ownable contract from OpenZeppelin is providing access mechanism where 
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions by 'onlyOwner' modifier.
+ */
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-    address public owner; // debug only
-
-    /**
-     * @dev Sets owner address to sender address
-     */
-    constructor() {
-        owner = msg.sender;
-    }
-
-    /**
-     * @dev Allows only the owner of contract to call specified functions
-     */
-    modifier ownerOnly {
-        require(msg.sender == owner);
-        _;
-    }
+contract Wallet is Ownable {
 
     /**
      * @dev Debug only contract balance getter function
@@ -28,12 +19,12 @@ contract Wallet {
     }
 
     /**
-     * @dev Withdraws wallet funds to specified address
+     * @dev Withdraws wallet funds to specified address, only owner may call this
      * @param _to Address to receive funds
      * @param _amount Amount of funds to withdraw
      */
-    function withdrawFunds(address payable _to, uint _amount) external {
-        require(address(this).balance <= _amount); // validate input
+    function withdrawFunds(address payable _to, uint _amount) external onlyOwner {
+        require(_amount <= address(this).balance); // validate input
         _to.transfer(_amount);
     }
 
